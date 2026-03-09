b85704a706042bc17fc28c3818978144154cef53
misamikasa <wayandevi813@gmail.com>
2026-03-09 21:34:09 +0800

    fix: bug App crashes after importing members from spreadsheet

--- src/hooks/useConfirmModal.ts
+++ src/hooks/useConfirmModal.ts
@@ -2,17 +2,22 @@ import ConfirmModalWrapper from '@components/Modal/Global/ConfirmModalWrapper';
 import type {ModalProps} from '@components/Modal/Global/ModalContext';
 import {useModal} from '@components/Modal/Global/ModalContext';
 
-type ConfirmModalOptions = Omit<React.ComponentProps<typeof ConfirmModalWrapper>, keyof ModalProps>;
+type ConfirmModalOptions = Omit<React.ComponentProps<typeof ConfirmModalWrapper>, keyof ModalProps> & {
+    /** Id of the modal. If passed, it will be used to deduplicate the modals. */
+    id?: string;
+};
 
 const useConfirmModal = () => {
     const context = useModal();
 
     const showConfirmModal = (options: ConfirmModalOptions) => {
+        const {id, ...restOptions} = options;
         return context.showModal({
             component: ConfirmModalWrapper,
+            id,
             props: {
                 shouldHandleNavigationBack: true,
-                ...options,
+                ...restOptions,
             },
         });
     };
